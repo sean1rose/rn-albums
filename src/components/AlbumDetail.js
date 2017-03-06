2a9932322388942d9a3eae0c43031d3efe1cc73e
Sean Rose <sean1rose@gmail.com>
2017-03-05 23:50:46 -0800

    start up AlbumDetail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+// functional component, just displaying album data
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = (props) => {
+  return (
+    <View>
+      <Text>{props.album.title}</Text>
+    </View>
+  );
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   // initializing state... (no constructor!)
@@ -15,7 +16,9 @@ class AlbumList extends Component {
 
   // helper method to generate list of albums...
   renderAlbums() {
-    return this.state.albums.map(album => <Text>{album.title}</Text>)
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
